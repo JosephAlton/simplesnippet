3260df5f24630c2dae7beeb1d6a3bc9d77a1ed0a
ParadigmZero <45772406+JosephAlton@users.noreply.github.com>
2025-07-15 22:54:29 +0100

    MVP done

---
+++ README.md
@@ -0,0 +1,3 @@
+# PromptStore
+
+Store prompt while you are writing it
---
+++ src/promptstore.tsx
@@ -0,0 +1,78 @@
+import { Form, Detail, LocalStorage, Action, ActionPanel, Clipboard, Icon, popToRoot, showToast, Toast } from "@raycast/api";
+import { useEffect, useState } from "react";
+
+export default function Command() {
+    const [prompt, setPrompt] = useState<string | null>(null);
+
+
+    async function copiedToastAndClose() {
+        await showToast({
+            style: Toast.Style.Success,
+            title: "Copied to clipboard",
+            message: "Prompt copied"
+        });
+        popToRoot({ clearSearchBar: true })
+    };
+
+
+    useEffect(() => {
+        async function loadPrompt() {
+            const savedPrompt = await LocalStorage.getItem("prompt");
+            setPrompt(typeof savedPrompt === "string" ? savedPrompt : "");
+        }
+        loadPrompt();
+    }, []);
+
+    if (prompt === null) {
+        return <Detail markdown="Loading..." />;
+    }
+
+    return (
+        <Form
+            actions={
+                <ActionPanel>
+                    <Action.CopyToClipboard
+                        title="Copy to clipboard (cut)"
+                        icon={Icon.Clipboard}
+                        shortcut={{ modifiers: ["cmd"], key: "return" }}
+                        onCopy={async () => {
+                            await LocalStorage.removeItem("prompt");
+                            await copiedToastAndClose();
+                        }}
+                        content={prompt}
+                    />
+
+                    <Action
+                        title="Clear"
+                        icon={Icon.Trash}
+                        shortcut={{ modifiers: ["cmd"], key: "backspace" }}
+                        onAction={async () => {
+                            setPrompt("");
+                            await LocalStorage.removeItem("prompt");
+                        }}
+                    />
+
+                    <Action
+                        title="Copy to clipboard (keep)"
+                        icon={Icon.Duplicate}
+                        shortcut={{ modifiers: ["cmd"], key: "m" }}
+                        onAction={async () => {
+                            await Clipboard.copy(prompt);
+                            await copiedToastAndClose();
+                        }}
+                    />
+
+                </ActionPanel>
+            }
+        >
+            <Form.TextArea
+                id="prompt"
+                value={prompt}
+                onChange={async (value) => {
+                    setPrompt(value);
+                    await LocalStorage.setItem("prompt", value);
+                }}
+            />
+        </Form>
+    );
+}
